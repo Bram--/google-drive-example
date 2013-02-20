b0f370cf88f10c2a082a5703ae90747590416a06
Bram Wijnands <bramwijnands@google.com>
2013-02-20 13:50:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleDrive::Application.config.secret_token = '0d442895b018438b39910182cbad55dedc8b5589e972ba7e3ee82bd089789d9a2a9d9c56c214adfac76d122b86f8966d14dba2960de670d633b951e1d04aba03'
